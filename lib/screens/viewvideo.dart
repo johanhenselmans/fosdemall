448ce97fa891c1cd23d0d856d4716131427cf41a
Johan Henselmans <johan@netsense.nl>
2024-10-30 18:06:00 +0100

    Changed from VLCPlayer to mediaKit, as it was far more efficient

--- lib/screens/viewvideo.dart
+++ lib/screens/viewvideo.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'package:fosdem/utils/settings_controller.dart';
 import 'package:fosdem/utils/style.dart';
 import 'package:go_router/go_router.dart';
+import 'package:media_kit/media_kit.dart';                      // Provides [Player], [Media], [Playlist] etc.
+import 'package:media_kit_video/media_kit_video.dart';          // Provides [VideoController] & [Video] etc.
 
-import 'package:fosdem/widgets/vlc_player_with_controls.dart';
-import 'package:simple_gesture_detector/simple_gesture_detector.dart';
+import '../utils/constants.dart';
 
 class ViewVideo extends StatefulWidget {
   SettingsController settingscontroller;
@@ -14,23 +14,17 @@ class ViewVideo extends StatefulWidget {
   static const routeName = 'viewvideo';
 
   ViewVideo(
-      {Key? key, required this.settingscontroller, required this.videoURL})
-      : super(key: key);
+      {super.key, required this.settingscontroller, required this.videoURL});
 
   @override
   _ViewVideoState createState() => _ViewVideoState();
 }
 
 class _ViewVideoState extends State<ViewVideo> {
-  VlcPlayerController? _controller;
-  final _key = GlobalKey<VlcPlayerWithControlsState>();
-
-  //
+  late final player = Player();
+  late final controller = VideoController(player);
 
   void _goback() async {
-    await _controller!.stopRecording();
-    await _controller!.stopRendererScanning();
-    await _controller!.dispose();
 
     GoRouter.of(context).pop();
     //GoRouter.of(context).go('/');
@@ -39,79 +33,49 @@ class _ViewVideoState extends State<ViewVideo> {
   @override
   void initState() {
     super.initState();
-
-    //
-    //
-    _controller = VlcPlayerController.network(
-      widget.videoURL,
-      hwAcc: HwAcc.full,
-      options: VlcPlayerOptions(
-        advanced: VlcAdvancedOptions([
-          VlcAdvancedOptions.networkCaching(2000),
-        ]),
-        subtitle: VlcSubtitleOptions([
-          VlcSubtitleOptions.boldStyle(true),
-          VlcSubtitleOptions.fontSize(30),
-          VlcSubtitleOptions.outlineColor(VlcSubtitleColor.yellow),
-          VlcSubtitleOptions.outlineThickness(VlcSubtitleThickness.normal),
-          // works only on externally added subtitles
-          VlcSubtitleOptions.color(VlcSubtitleColor.navy),
-        ]),
-        http: VlcHttpOptions([
-          VlcHttpOptions.httpReconnect(true),
-        ]),
-        rtp: VlcRtpOptions([
-          VlcRtpOptions.rtpOverRtsp(true),
-        ]),
-      ),
-    );
-    _controller!.addOnInitListener(() async {
-      await _controller!.startRendererScanning();
-    });
-    _controller!.addOnRendererEventListener((type, id, name) {
-      print('OnRendererEventListener $type $id $name');
-    });
+    player.open(Media(widget.videoURL));
   }
 
   @override
   Widget build(BuildContext context) {
+    double aheight = MediaQuery.of(context).size.height;
+    double awidth = MediaQuery.of(context).size.width;
+    if (MediaQuery.of(context).orientation == Orientation.landscape) {
+      aheight = MediaQuery.of(context).size.height * 11 / 13.0;
+    }else {
+      aheight = MediaQuery.of(context).size.width * 9.0 / 16.0;
+    };
     return SafeArea(
       child: Scaffold(
-        body: ListView(padding: const EdgeInsets.all(24), children: [
-        Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-            children: [
-              ElevatedButton(
-                style: fosdemElevatedButtonStyle,
-                child: Row(
-                  children: [
-                    Icon(Icons.arrow_back_outlined),
-                    const Text(
-                      "Back",
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+        body: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children:[
+          ElevatedButton(
+            style: fosdemElevatedButtonStyle,
+            child: const Row(
+              children: [
+                Icon(Icons.arrow_back_outlined, color: fosdemColorButtonTekst),
+                Text(
+                  "Back",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: fosdemColorButtonTekst),
                 ),
-                onPressed: () => _goback(),
-              ),
-              ]
-        ),
-          SizedBox(
-            height: 400,
-            child: VlcPlayerWithControls(
-              key: _key,
-              controller: _controller!,
+              ],
             ),
+            onPressed: () => _goback(),
           ),
-          SimpleGestureDetector(
-              onTap: () async {
-                await _controller!.setMediaFromNetwork(
-                  widget.videoURL,
-                  hwAcc: HwAcc.full,
-                );
-              },
-              child: Container()),
+        ]
+          ),
+          Expanded(child:ListView(padding: const EdgeInsets.all(6), children: [
+            SizedBox(
+                width: awidth,
+                height: aheight,
+              child:
+              Video(controller: controller),
+            ),
+
+
+          ])
+          )
         ]),
       ),
     );
@@ -119,9 +83,7 @@ class _ViewVideoState extends State<ViewVideo> {
 
   @override
   void dispose() async {
+    player.dispose();
     super.dispose();
-    //await _controller!.stopRecording();
-    //await _controller!.stopRendererScanning();
-    //await _controller!.dispose();
   }
 }
